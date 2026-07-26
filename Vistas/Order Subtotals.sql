fd09cc6b161d54b0de4a0f50de71c1ebf09f705d
Melissa <Melissa@DESKTOP-L5GK60G>
2026-07-26 18:53:25 -0400

    Agregar archivos de proyecto.

---
+++ Vistas/Order Subtotals.sql
@@ -0,0 +1,5 @@
+﻿
+create view "Order Subtotals" AS
+SELECT "OrderDetails".OrderID, Sum(CONVERT(money,("OrderDetails".UnitPrice*Quantity*(1-Discount)/100))*100) AS Subtotal
+FROM "OrderDetails"
+GROUP BY "OrderDetails".OrderID
